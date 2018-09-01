956c08af1b1baaf28d5901d8aaa4f5a670ca1ceb
Patricio Andrés Peña Muñoz <patriciopenamunoz@gmail.com>
2018-09-01 07:43:47 -0300

    Ajustar listado de empleos del empleador

--- app/controllers/jobs_controller.rb
+++ app/controllers/jobs_controller.rb
@@ -41,7 +41,7 @@ class JobsController < ApplicationController
 
     respond_to do |format|
       if @job.save
-        format.html { redirect_to @job, notice: 'Job was successfully created.' }
+        format.html { redirect_to root_path }
         format.json { render :show, status: :created, location: @job }
       else
         format.html { render :new }
